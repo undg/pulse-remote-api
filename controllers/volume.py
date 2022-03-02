feb466ae23c604a097d190726079d50b1ae7280f
und3rdg <und3rdg@gmail.com>
2022-03-02 21:25:43 +0000

    Set volume endpoint.

--- controllers/volume.py
+++ controllers/volume.py
@@ -3,27 +3,30 @@ from models.sink import sink_serialize
 from enum import Enum
 
 
-class Change(Enum):
-    UP = "UP"
-    DOWN = "DOWN"
-    TOGGLE = "MUTE"
-    INFO = "INFO"
+class Value(Enum):
+    UP = "up"
+    DOWN = "down"
+    TOGGLE = "toggle"
+    INFO = "info"
+    SET = "set"
 
 
-def volume(change: Change):
+def volume(change: Value, vol: int = 0):
 
     sinks = []
 
     pulse = pulsectl.Pulse("volume-changer")
     for sink in pulse.sink_list():
         # Volume is usually in 0-1.0 range, with >1.0 being soft-boosted
-        if change == Change.UP:
-            pulse.volume_change_all_chans(sink, 0.1)
-        elif change == Change.DOWN:
-            pulse.volume_change_all_chans(sink, -0.1)
-        elif change == Change.INFO:
+        if change == Value.UP:
+            pulse.volume_change_all_chans(sink, 0.05)
+        elif change == Value.DOWN:
+            pulse.volume_change_all_chans(sink, -0.05)
+        elif change == Value.INFO:
             pulse.volume_change_all_chans(sink, 0) # possibly pulse.volume_get_all_chans() is better, if it returns same type.
-        elif change == Change.TOGGLE:
+        elif change == Value.SET:
+            pulse.volume_set_all_chans(sink, vol / 100)
+        elif change == Value.TOGGLE:
             if sink.mute == 1:
                 pulse.mute(sink, 0)
             else:
@@ -33,13 +36,16 @@ def volume(change: Change):
     return sinks
 
 def volume_down():
-    return volume(Change.DOWN)
+    return volume(Value.DOWN)
 
 def volume_up():
-    return volume(Change.UP)
+    return volume(Value.UP)
 
 def volume_toggle():
-    return volume(Change.TOGGLE)
+    return volume(Value.TOGGLE)
 
 def volume_info():
-    return volume(Change.INFO)
+    return volume(Value.INFO)
+
+def volume_set(vol: int):
+    return volume(Value.SET, vol)
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import sys
 from fastapi import FastAPI
-from controllers.volume import volume_up, volume_down, volume_toggle, volume_info
+from controllers.volume import volume_set, volume_up, volume_down, volume_toggle, volume_info
 from controllers.cors import setupCORS
 
 sys.path.append(".")
 app = FastAPI()
 setupCORS(app)
 
+@app.get("/volume/set/{vol}")
+def vol_set(vol: str):
+    return volume_set(int(vol))
 
 @app.get("/volume/up")
 def vol_up():
     return volume_up()
-
 
 @app.get("/volume/down")
 def vol_down():
@@ -26,3 +28,4 @@ def vol_mute():
 @app.get("/volume/info")
 def vol_info():
     return volume_info()
+
